fac45dbd2dccc349ccae998ff839b66360abff79
Noah <tw110624@students.musd.org>
2025-10-15 16:46:52 -0700

    15 October 2025, 4:45 P.M.

---
+++ THEREALREALREALJAVASTUF.java
@@ -0,0 +1,44 @@
+public class THEREALREALREALJAVASTUF{
+    public static void main(String[] args){
+        System.out.println("hello yes u have reached dado house of fine pharmaceutical also laundry and tan. please also visit dado short term loan and vhs rental store while in town.\nyes i am dado of course purveyor of fine dado business. pls forgive spelling error hamster is often walk about the keyboard and lkuglkjm;olfbjkinjuk hamstr\noh u r interest in see cat log of valuable remedy? yes i show this 2 u. dado has what fixes the ails in u. pls see cat log below. again sry for any mispells, dado uppercase key broke.");
+    }
+}
+class Vehicle{
+    private int mo;
+    private double cpu;
+    private double ats;
+    private String nom;
+    private double sel;
+    public Vehicle(int mop, double cpup, double atsp, String nomp, double selp){
+            mo = mop;
+            cpu = cpup;
+            ats = atsp;
+            nom = nomp;
+            sel = selp;
+    }
+    public double mvssp(){
+        return(sel-cpu);
+    }
+    public int getmo(){
+        return(mo);
+    }
+    public double getats(){
+        return(ats);
+    }
+    public double getsel(){
+        return(sel);
+    }
+    public String getnom(){
+        return(nom);
+    }
+}
+class Car extends Vehicle{
+    private int mo;
+    private float cpu;
+    private float ats;
+    private String nom;
+    private float sel;
+    public Car(int mop, double cpup, double atsp, String nomp, double selp){
+            super(mop,cpup, atsp,nomp, selp);
+            }
+    }
